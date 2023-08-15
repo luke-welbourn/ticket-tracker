e7fbf1db712f24a51330e331c1357842aa82a6cc
Luke Welbourn <luke.tarquin5@outlook.com>
2023-08-15 11:43:19 +0100

    added 1 dynamically generated card using team info.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import "./App.scss";
 import team from "./data/team";
+import Ticket from "./components/Ticket/Ticket";
+
+const firstTicket = team[0];
 
 const App = () => {
   return (
     <div className="app">
       <header>
-        <h2>Ticket Tracker</h2>
+        <h1>Ticket Tracker</h1>
       </header>
       <main>
-        <section className="ticket-container"></section>
+        <section className="ticket-container">
+          <Ticket
+            id={firstTicket.id}
+            name={firstTicket.name}
+            role={firstTicket.role}
+          />
+        </section>
       </main>
     </div>
   );
--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,3 +1,27 @@
 import { useState } from "react";
 
 import { teamMember, team } from "../../data/team";
+
+import "./Ticket.scss";
+
+type ticketType = {
+  id: number;
+  name: string;
+  role: string;
+};
+
+const Ticket = ({ id, name, role }: ticketType) => {
+  //
+  //
+  return (
+    <div className="ticket-tile" key={id}>
+      <p>Name: {name}</p>
+      <p>Role: {role}</p>
+      <div>
+        <p>Counter:</p>
+      </div>
+    </div>
+  );
+};
+
+export default Ticket;
